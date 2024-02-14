ea06b70477a03ef4e602fe6924b13cbad2e32b21
Mohd Mujahed <105550939+Mohd-Mujahed@users.noreply.github.com>
2024-02-15 05:23:12 +0530

    Update api.ts

--- axiosUtility/api.ts
+++ axiosUtility/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'https://aplus-laundry-backend.vercel.app/',
-  baseURL: 'http://localhost:4040/',
+  baseURL: 'https://aplus-laundry-backend.vercel.app/',
 });
 
 
